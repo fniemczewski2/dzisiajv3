6f7b0bdfec6db9053a4a5a758347c13153c9d068
f.niemczewski2 <spam.fmn@gmail.com>
2026-01-18 01:46:53 +0100

    Ver 1.15.4 Fix: Authguard fixed

--- components/AuthGuard.tsx
+++ components/AuthGuard.tsx
@@ -8,11 +8,10 @@ import LoadingState from "./LoadingState";
 export default function AuthGuard({ children }: { children: ReactNode }) {
   const [loading, setLoading] = useState(true);
   const session = useSession();
-  const userEmail = session?.user.email || process.env.NEXT_PUBLIC_USER_EMAIL;
   const router = useRouter();
 
   const checkAuth = useCallback(() => {
-    if (session === null && userEmail === "") {
+    if (session === null) {
        const next = encodeURIComponent(router.asPath);
        router.replace(`/login?next=${next}`, undefined, { shallow: true });
     }
