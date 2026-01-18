0d44735e5d57c4cd39ae9d42a6e7513bf4919825
f.niemczewski2 <spam.fmn@gmail.com>
2026-01-18 01:41:40 +0100

    Ver. 1.15.3 fix: next-pwa fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withPWA = require("next-pwa")({
   dest: "public",
+  buildExcludes: [/./],
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development", // PWA off in dev
