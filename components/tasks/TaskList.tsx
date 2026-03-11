66318080b8465124be60e183ba9eb1740f2c96d3
f.niemczewski2 <spam.fmn@gmail.com>
2026-03-11 01:13:27 +0100

    Ver 1.23.1 Del: TaskTimer deleted

--- components/tasks/TaskList.tsx
+++ components/tasks/TaskList.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import TaskItem from "./TaskItem";
-import TaskTimer from "./TaskTimer";
 import { Task } from "../../types";
 import NoResultsState from "../NoResultsState";
 
@@ -13,7 +12,6 @@ export default function TaskList({
   tasks,
   onTasksChange,
 }: Props) {
-  const [activeTask, setActiveTask] = useState<Task | null>(null);
 
   if (tasks.length === 0) {
     return (
@@ -23,15 +21,6 @@ export default function TaskList({
 
   return (
     <div className="space-y-4">
-      {activeTask && (
-        <TaskTimer
-          task={activeTask}
-          onComplete={() => {
-            setActiveTask(null);
-            onTasksChange(); 
-          }}
-        />
-      )}
       <ul className="flex flex-wrap mt-6 gap-4 sm:gap-6">
         {tasks.map((task) => (
           <li key={task.id} className="w-full md:w-[calc(50%-12px)] lg:w-[calc(33.333%-16px)]">
