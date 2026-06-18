8fdd27e37679785af333455bb2861eb54a46ba3f
f.niemczewski2 <spam.fmn@gmail.com>
2026-06-18 19:26:39 +0200

    1.33.13

--- components/people/ExportPeople.tsx
+++ components/people/ExportPeople.tsx
@@ -64,7 +64,7 @@ export const ExportPeople = ({ people }: ExportProps) => {
       onClick={handleExport}
       className="px-4 py-2 bg-surface hover:bg-surfaceHover text-textSecondary font-medium rounded-lg flex items-center gap-2 border border-gray-200 dark:border-gray-800 transition-colors"
     >
-      <Download className="w-5 h-5" /> .csv
+      .csv <Download className="w-5 h-5" />
     </button>
   );
 };
